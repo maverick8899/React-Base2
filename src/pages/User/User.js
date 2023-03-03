80760f9b0babbd746d27e98c06a8c2d08a95ae12
Kimbang89 <trankimbang0809@gmail.com>
2023-03-03 23:13:35 +0700

    xóa video thử để deploy trên netify

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -5,13 +5,13 @@ import * as backend from "../../services/backend";
 
 function User() {
   //chạy localhost8080 express trả về json=> dùng như api
-  useEffect(() => {
-    fetchAPI();
-  });
-  const fetchAPI = async () => {
-    const data = await backend.getData();
-    console.log(data);
-  };
+  // useEffect(() => {
+  //   fetchAPI();
+  // });
+  // const fetchAPI = async () => {
+  //   const data = await backend.getData();
+  //   console.log(data);
+  // };
   return (
     <div className={styles.container}>
       <div className={styles.user}>Page User</div>
